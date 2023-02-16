aa9269185ec53b7b2fa52ab69a8e1cf2bd05bb16
Mukesh Korat <mukeshkumar.korat@aurea.com>
2023-02-16 18:57:43 +0530

    Added emulator closing logic

--- e2e/suite2.test.ts
+++ e2e/suite2.test.ts
@@ -7,17 +7,18 @@ describe('DetoxTest suite - 2', () => {
 
   it('should show Step One', async () => {
     await expect(element(by.text('Step One'))).toBeVisible();
-    console.log('***************    Step One       ***********');
+    console.log('***************    Step One      ***********');
   });
 
   it('should show Debug', async () => {
     // await element(by.id('hello_button')).tap();
     await expect(element(by.text('Debug'))).toBeVisible();
-    console.log('***************    Debug       ***********');
+    console.log('***************    Debug      ***********');
   });
 
-  // it('should show Learn more', async () => {
-  //   // await element(by.id('world_button')).tap();
-  //   await expect(element(by.text('Learn More'))).toBeVisible();
-  // });
+  it('should show Learn more', async () => {
+    // await element(by.id('world_button')).tap();
+    await expect(element(by.text('Learn More'))).toBeVisible();
+    console.log('***************    Learn More       ***********');
+  });
 });
